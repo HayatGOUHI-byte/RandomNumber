27c7b7e31e9679cf821dfc839575158d2a831404
h120ayat <93711127+girlcoder3@users.noreply.github.com>
2022-02-19 11:07:15 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace RandomNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "hayat";
+           
+            Console.WriteLine(name + " " +prenom);
+        }
+    }
+
+
+   
+}
