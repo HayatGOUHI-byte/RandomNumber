852ae84e23d83ade5b5260be41647e65a6e049d3
h120ayat <93711127+girlcoder3@users.noreply.github.com>
2022-02-20 14:59:43 +0000

    c

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace RandomNumber
         static void Main(string[] args)
         {
             string appName = "Number Guesser";
-            string appVersion = "1.0.2";
+            string appVersion = "1.0.2"
             string appAuthor = "brad Traversey";
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("{0} is realesed By {1} on vesrion",appName, appVersion, appAuthor);
